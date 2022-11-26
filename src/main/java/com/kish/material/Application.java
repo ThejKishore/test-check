8d2e5bda6c62f01e0f76c24cfa734152346b4991
start.vaadin.com <no-reply@vaadin.com>
2022-11-26 01:24:25 +0000

    Generated project

---
+++ src/main/java/com/kish/material/Application.java
@@ -0,0 +1,30 @@
+package com.kish.material;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/roboto", version = "4.5.0")
+@Theme(value = "test")
+@PWA(name = "Test", shortName = "Test", offlineResources = {})
+@EntityScan(basePackageClasses = {Application.class})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/kish/material/data/service/SamplePersonRepository.java
@@ -0,0 +1,9 @@
+package com.kish.material.data.service;
+
+import com.kish.material.data.entity.SamplePerson;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
+
+}
